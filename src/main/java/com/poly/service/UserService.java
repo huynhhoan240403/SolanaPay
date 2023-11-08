687a0842c25cb2b171f9b7a839041d201ed0fb61
huynhhoan240403 <hoanhuynh240403@gmail.com>
2023-11-08 15:33:38 +0700

    update

--- src/main/java/com/poly/service/UserService.java
+++ src/main/java/com/poly/service/UserService.java
@@ -46,15 +46,4 @@ public class UserService implements UserDetailsService{
 			throw new UsernameNotFoundException(username + "not found");
 		}
 	}
-	
-//	public void loginFromOAuth2(OAuth2AuthenticationToken oauth2){
-//		// String fullname = oauth2.getPrincipal().getAttribute("name");
-//		String email = oauth2.getPrincipal().getAttribute("email");
-//		String password = Long.toHexString(System.currentTimeMillis());
-//		
-//		UserDetails user = User.withUsername(email)
-//				.password(pe.encode(password)).roles("0").build();
-//		Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-//		SecurityContextHolder.getContext().setAuthentication(auth);
-//	}
 }
